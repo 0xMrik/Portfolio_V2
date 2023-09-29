d0cf662292b59bfb12b3f6fa043a0f87311ef635
0xMrik <agu.aymeric@hotmail.fr>
2023-09-29 22:53:49 +0200

    first commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,122 @@
 import React from 'react';
+// motion 
+import { motion } from 'framer-motion';
+// variants 
+import { fadeIn } from '../variants';
+// img
+import Img1 from '../assets/portfolio-img1.png'
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0 justify-center"
+          >
+            {/* text */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My Latest <br />
+                Work
+              </h2>
+              <p className="max-w-sm mb-16">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi,
+                nam in? Tempora sapiente neque enim. Animi sint eaque
+                perferendis
+              </p>
+              <button
+                className="btn btn-sm"
+                data-text="View all projects"
+              />
+            </div>
+          </motion.div>
+          <div className="flex-1 flex flex-col gap-y-10">
+            {/* image */}
+            <motion.div
+              variants={fadeIn("left", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+              className="group relative overflow-hidden border-2 border-white/50 rounded-xl"
+            >
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img2}
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </motion.div>
+            {/* image */}
+            <motion.div
+              variants={fadeIn("left", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+              className="group relative overflow-hidden border-2 border-white/50 rounded-xl"
+            >
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </motion.div>
+            {/* image */}
+            <motion.div
+              variants={fadeIn("left", 0.9)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+              className="group relative overflow-hidden border-2 border-white/50 rounded-xl"
+            >
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
